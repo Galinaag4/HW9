e2a53469c028a95bf80d68e04e51f5b12f18d74e
Galinaag4 <Galinaag@yandex.ru>
2022-08-24 19:59:08 +0300

    first commit

---
+++ src/Book.java
@@ -0,0 +1,12 @@
+public class Book {
+    private String nameBook;
+    private int age;
+
+    public Book(String nameBook, int age) {
+        this.nameBook = nameBook;
+        this.age = age;}
+
+    public int setAge() {
+        return this.age;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        Book One=new Book("ABC",1875);
+        Book Two=new Book("123",2022);
+        Author BookOne = new Author("vinni","pooh");
+        Author BookTwo = new Author("Kristopher","Robin");
 
 
+        System.out.println();
     }
+
 }
