582910f3e9f1500a471c6db285c1364bb2a8847e
Galinaag4 <Galinaag@yandex.ru>
2022-08-24 20:00:36 +0300

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Book One=new Book("ABC",1875);
-        Book Two=new Book("123",2022);
+        Book One= new Book("ABC",1875);
+        Book Two= new Book("123",2022);
         Author BookOne = new Author("vinni","pooh");
         Author BookTwo = new Author("Kristopher","Robin");
 
