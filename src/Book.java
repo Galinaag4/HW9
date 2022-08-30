aa4a5d2319cccd733a4eee06936e6ae29d1aea1b
Galinaag4 <Galinaag@yandex.ru>
2022-08-30 18:51:45 +0300

    Hash code, toString, equals

--- src/Book.java
+++ src/Book.java
@@ -1,12 +1,50 @@
+import java.util.Objects;
+
 public class Book {
     private String nameBook;
     private int age;
 
-    public Book(String nameBook, int age) {
-        this.nameBook = nameBook;
-        this.age = age;}
+    private String author;
 
+    public Book(String nameBook, String author, int age) {
+        this.author = author;
+        this.nameBook = nameBook;
+        this.age = age;
+    }
+
+
+    public String getNameBook() {
+        return this.nameBook;
+    }
+    public int getAge() {
+        return this.age;
+    }
+    public String getAuthor() {
+        return this.author;
+    }
     public int setAge() {
         return this.age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return age == book.age && Objects.equals(nameBook, book.nameBook) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameBook, age, author);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "nameBook='" + nameBook + '\'' +
+                ", age=" + age +
+                ", author='" + author + '\'' +
+                '}';
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Book One= new Book("ABC",1875);
-        Book Two= new Book("123",2022);
-        Author BookOne = new Author("vinni","pooh");
-        Author BookTwo = new Author("Kristopher","Robin");
+        Book one= new Book("Классика","Борис Виан",1974);
+        Author BookOne = new Author("Борис","Виан");
 
 
-        System.out.println();
+
+        System.out.println(one.getNameBook()+one.getAge()+one.getAuthor());
     }
 
 }
